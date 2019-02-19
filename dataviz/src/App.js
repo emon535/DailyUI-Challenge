03ef6529fbe2c66196dad7f846c4af17766a595d
Foysal Ahmed Emon <foysal.ahmed@infolytx.com>
2019-02-19 14:35:05 +0600

    Changed Branch

---
+++ dataviz/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {ScaleLinear} from 'd3-scale';
+import {max} from 'd3-array';
+import {select} from 'd3-selection';
+
+
+ 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
