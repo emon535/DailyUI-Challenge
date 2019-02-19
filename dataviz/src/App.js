ce2fa48a7f3e47f404b2f62d5571a7e249068ba4
Foysal Ahmed Emon <emon535@gmail.com>
2019-02-19 14:41:38 +0600

    Git Testing

--- dataviz/src/App.js
+++ dataviz/src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Testing Git.
           </a>
         </header>
       </div>
